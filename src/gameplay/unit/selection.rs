f96c75ad55fcf30f7606e1df3409e7ba7be6706b
rapuska <mikalajsiatkouski@gmail.com>
2025-01-20 23:06:54 +0300

    highlight selected unit

--- src/gameplay/unit/selection.rs
+++ src/gameplay/unit/selection.rs
@@ -1,14 +1,19 @@
 use crate::gameplay::unit::selection::feature::SelectedUnit;
 use crate::prelude::*;
 use crate::prelude::selection::feature::mark_clicked_units_as_selected;
+use crate::prelude::selection::view::SelectionViewPlugin;
 
 pub mod feature;
+
+mod view;
 
 pub struct UnitSelectionPlugin;
 
 impl Plugin for UnitSelectionPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_plugins(SelectionViewPlugin)
+
             .add_systems(Update, mark_clicked_units_as_selected.in_set(Order::Preprocess))
         ;
     }
---
+++ src/gameplay/unit/selection/view.rs
@@ -0,0 +1,25 @@
+use crate::prelude::*;
+use crate::prelude::selection::feature::SelectedUnit;
+
+pub struct SelectionViewPlugin;
+
+impl Plugin for SelectionViewPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            .add_systems(Update, paint_selected_unit.in_set(Order::Postprocess))
+        ;
+    }
+}
+
+fn paint_selected_unit(
+    mut units: Query<(&mut Sprite, Option<&SelectedUnit>), (With<UnitID>)>,
+) {
+    for (mut sprite, selected) in units.iter_mut() {
+        let highlighted_color = Srgba::new(1.0, 1.0, 0.5, 1.0);
+
+        sprite.color = match selected {
+            None => Color::Srgba(Srgba::WHITE),
+            Some(_) => Color::Srgba(highlighted_color),
+        };
+    }
+}
