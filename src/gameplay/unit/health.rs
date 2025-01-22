33140002ac7962513b6120854fa857c5158117a9
rapuska <mikalajsiatkouski@gmail.com>
2025-01-22 09:38:44 +0300

    health plugin

---
+++ src/gameplay/unit/health.rs
@@ -0,0 +1,13 @@
+use crate::prelude::*;
+
+mod feature;
+
+pub struct HealthPlugin;
+
+impl Plugin for HealthPlugin {
+    fn build(&self, app: &mut App) {
+        app
+
+        ;
+    }
+}
---
+++ src/gameplay/unit/health/feature/components.rs
@@ -0,0 +1,7 @@
+use crate::prelude::*;
+
+#[derive(Component)]
+pub struct MaxHealth(pub f32);
+
+#[derive(Component)]
+pub struct Health(pub f32);
