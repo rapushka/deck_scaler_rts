089754b3d8ebbc590f607e4efd8816599d79ed7b
rapuska <mikalajsiatkouski@gmail.com>
2025-01-22 09:46:28 +0300

    add max health as stat

--- src/gameplay/unit/stats/structs.rs
+++ src/gameplay/unit/stats/structs.rs
@@ -10,12 +10,14 @@ pub enum Stat {
     MovementSpeed,
     Range,
     AttackChargeDuration,
+    MaxHealth,
 }
 
 pub struct StatProps<T> {
     pub movement_speed: T,
     pub range: T,
     pub attack_charge_duration: T,
+    pub max_health: T,
 }
 
 impl<T: Default> Default for StatProps<T> {
@@ -24,6 +26,7 @@ impl<T: Default> Default for StatProps<T> {
             movement_speed: T::default(),
             range: T::default(),
             attack_charge_duration: T::default(),
+            max_health: T::default(),
         }
     }
 }
@@ -41,6 +44,7 @@ impl<T: Default> StatsMap<T> {
         stats.insert(Stat::MovementSpeed, props.movement_speed);
         stats.insert(Stat::Range, props.range);
         stats.insert(Stat::AttackChargeDuration, props.attack_charge_duration);
+        stats.insert(Stat::MaxHealth, props.max_health);
 
         StatsMap(stats)
     }
