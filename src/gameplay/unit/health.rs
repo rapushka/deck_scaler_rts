df650272a24894683bf2648c74f56e51ce61e8cb
rapuska <mikalajsiatkouski@gmail.com>
2025-01-23 09:33:44 +0300

    reflect health

--- src/gameplay/unit/health.rs
+++ src/gameplay/unit/health.rs
@@ -11,6 +11,8 @@ pub struct HealthPlugin;
 impl Plugin for HealthPlugin {
     fn build(&self, app: &mut App) {
         app
+            .register_type::<Health>()
+
             .add_plugins(HealthViewPlugin)
         ;
     }
--- src/gameplay/unit/health/feature/components.rs
+++ src/gameplay/unit/health/feature/components.rs
@@ -3,5 +3,6 @@ use crate::prelude::*;
 #[derive(Component)]
 pub struct MaxHealth(pub f32);
 
-#[derive(Component)]
+#[derive(Component, Reflect)]
+#[reflect(Component)]
 pub struct Health(pub f32);
